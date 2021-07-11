9c27a67688190f0fda3ee9e4f6b59034e6073478
codewire8 <81359508+codewire8@users.noreply.github.com>
2021-07-11 11:56:00 +0800

    Ng-If

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'vidly';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.movies = [
+        {
+          title: 'Spider Man',
+          releaseDate: new Date('2016-11-14'),
+          price: 1400.99,
+        },
+        {
+          title: 'Moana',
+          releaseDate: new Date('2017-06-22'),
+          price: 1500.99,
+        },
+      ];
+    }, 1000);
+  }
 
-  movies = [
-    {
-      title: 'Spider Man',
-      releaseDate: new Date('2016-11-14'),
-      price: 1400.99,
-    },
-    {
-      title: 'Moana',
-      releaseDate: new Date('2017-06-22'),
-      price: 1500.99,
-    },
-  ];
+  title = 'vidly';
+  movies;
 
   duplicateNumber(n: number) {
     return n * 2;
