530ea4e071c32baaf21210e868ec0df5c5418db7
codewire8 <81359508+codewire8@users.noreply.github.com>
2021-07-11 11:11:28 +0800

    Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'vidly';
+
+  movie = {
+    title: 'Spider Man',
+    releaseDate: new Date(),
+  };
+
+  duplicateNumber(n: number) {
+    return n * 2;
+  }
 }
