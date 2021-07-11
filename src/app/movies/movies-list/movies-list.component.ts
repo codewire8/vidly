e9be625c5b2178d918197237c3d3051591387134
codewire8 <81359508+codewire8@users.noreply.github.com>
2021-07-11 13:04:48 +0800

    Events

--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -3,15 +3,17 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
-  styleUrls: ['./movies-list.component.css']
+  styleUrls: ['./movies-list.component.css'],
 })
 export class MoviesListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   @Input()
   movies;
+
+  remove(index: number) {
+    this.movies.splice(index, 1);
+  }
 }
