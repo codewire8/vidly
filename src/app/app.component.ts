370dac7a6ac067700cf014632b2936699f8b1242
codewire8 <81359508+codewire8@users.noreply.github.com>
2021-07-11 21:18:39 +0800

    Movie Poster Clean Up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,16 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.moviesInTheaters = [
       {
-        title: 'Spider Man',
+        title: 'No Sudden Move',
         releaseDate: new Date('2016-11-14'),
         price: 1400.99,
+        poster: 'https://www.joblo.com/assets/images/joblo/posters/2021/06/no-sudden-move_thumb.jpg'
       },
       {
-        title: 'Moana',
+        title: 'Shang-Chi',
         releaseDate: new Date('2017-06-22'),
         price: 1500.99,
+        poster: 'https://www.joblo.com/assets/images/joblo/posters/2021/06/Shang-Chi-poster-4062076_thumb.jpg'
       },
     ];
     this.moviesFutureReleases = [];
