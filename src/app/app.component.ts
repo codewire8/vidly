1fb2f5be88186740206431c062cb864a2dc16dcf
codewire8 <81359508+codewire8@users.noreply.github.com>
2021-07-11 12:13:16 +0800

    Component - Parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
-   
+    this.moviesInTheaters = [
+      {
+        title: 'Spider Man',
+        releaseDate: new Date('2016-11-14'),
+        price: 1400.99,
+      },
+      {
+        title: 'Moana',
+        releaseDate: new Date('2017-06-22'),
+        price: 1500.99,
+      },
+    ];
+    this.moviesFutureReleases = [];
   }
-
-  title = 'vidly';
-  movies;
-
-  duplicateNumber(n: number) {
-    return n * 2;
-  }
+  moviesInTheaters;
+  moviesFutureReleases;
 }
--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-movies-list',
@@ -10,21 +10,8 @@ export class MoviesListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.movies = [
-        {
-          title: 'Spider Man',
-          releaseDate: new Date('2016-11-14'),
-          price: 1400.99,
-        },
-        {
-          title: 'Moana',
-          releaseDate: new Date('2017-06-22'),
-          price: 1500.99,
-        },
-      ];
-    }, 1000);
   }
-  movies;
 
+  @Input()
+  movies;
 }
