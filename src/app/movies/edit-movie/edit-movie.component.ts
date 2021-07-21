d014ad277d8d7c6888322ad83ed30e78f80c03a3
Marvin Fordan <marvin@sdsolutions.com.ph>
2021-07-21 20:12:20 +0800

    Create movie form

--- src/app/movies/edit-movie/edit-movie.component.ts
+++ src/app/movies/edit-movie/edit-movie.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { movieCreationDTO, movieDTO } from '../movies.model';
 
 @Component({
   selector: 'app-edit-movie',
@@ -9,7 +10,20 @@ import { ActivatedRoute } from '@angular/router';
 export class EditMovieComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute) {}
 
+  model: movieDTO = {
+    title: 'Spider Man',
+    inTheaters: true,
+    summary: 'something',
+    releaseDate: new Date(),
+    trailer: 'ghgasdxhgsuydg',
+    poster: 'https://m.media-amazon.com/images/M/MV5BNTMxOGI4OGMtMTgwMy00NmFjLWIyOTUtYjQ0OGQ4Mjk0YjNjXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX67_CR0,0,67,98_AL_.jpg',
+  };
+
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {});
+  }
+
+  saveChanges(movieCreationDTO: movieCreationDTO) {
+
   }
 }
