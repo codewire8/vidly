f8644f5c5f061aeb608d41be115f303ca009f22a
Marvin Fordan <marvin@sdsolutions.com.ph>
2021-07-21 22:56:45 +0800

    Autocomplete

---
+++ src/app/actors/actors-autocomplete/actors-autocomplete.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+@Component({
+  selector: 'app-actors-autocomplete',
+  templateUrl: './actors-autocomplete.component.html',
+  styleUrls: ['./actors-autocomplete.component.css'],
+})
+export class ActorsAutocompleteComponent implements OnInit {
+  constructor() {}
+
+  control: FormControl = new FormControl();
+
+  actors = [
+    {
+      name: 'Tom Holland',
+      picture:
+        'https://m.media-amazon.com/images/M/MV5BNzZiNTEyNTItYjNhMS00YjI2LWIwMWQtZmYwYTRlNjMyZTJjXkEyXkFqcGdeQXVyMTExNzQzMDE0._V1_UX67_CR0,0,67,98_AL_.jpg',
+    },
+    {
+      name: 'Tom Hanks',
+      picture:
+        'https://m.media-amazon.com/images/M/MV5BMTQ2MjMwNDA3Nl5BMl5BanBnXkFtZTcwMTA2NDY3NQ@@._V1_UY317_CR2,0,214,317_AL_.jpg',
+    },
+    {
+      name: 'Keanu Reeves',
+      picture:
+        'https://m.media-amazon.com/images/M/MV5BYTkzODI4MDMtNDNmZC00NDZlLWFmNTktNDRhOWE2YzhlZTQ2XkEyXkFqcGdeQXVyMTE1MTYxNDAw._V1_UY317_CR19,0,214,317_AL_.jpg',
+    },
+  ];
+
+  selectedActors = [];
+  originalActors = this.actors;
+
+  ngOnInit(): void {
+    this.control.valueChanges.subscribe((value) => {
+      this.actors = this.originalActors;
+      this.actors = this.actors.filter(
+        (actor) => actor.name.indexOf(value) !== -1
+      );
+    });
+  }
+
+  optionSelected(event: MatAutocompleteSelectedEvent) {
+    this.selectedActors.push(event.option.value);
+    this.control.patchValue('');
+  }
+}
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -10,6 +10,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
   declarations: [],
@@ -24,6 +25,7 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatDatepickerModule,
     MatNativeDateModule,
     MatTabsModule,
+    MatAutocompleteModule
   ],
   imports: [CommonModule],
 })
