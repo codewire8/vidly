f3b784c2879b9542872577583b688239b99b2767
Marvin Fordan <marvin@sdsolutions.com.ph>
2021-07-24 13:22:57 +0800

    Service

--- src/app/genres/index-genre/index-genre.component.ts
+++ src/app/genres/index-genre/index-genre.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { GenresService } from '../genres.service';
 
 @Component({
   selector: 'app-index-genre',
   templateUrl: './index-genre.component.html',
-  styleUrls: ['./index-genre.component.css']
+  styleUrls: ['./index-genre.component.css'],
 })
 export class IndexGenreComponent implements OnInit {
-
-  constructor() { }
+  constructor(private genresService: GenresService) {}
 
   ngOnInit(): void {
+    const genres = this.genresService.getAll();
+    console.log(genres)
   }
-
 }
